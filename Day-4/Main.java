44b0cde52813fe47f02baeb77e2fb30333bc5eb9
ShevinuM <nsmcooray@mun.ca>
2023-12-04 08:31:37 -0330

    Completed part 1

---
+++ Day-4/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> input = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                input.add(line);
+            }
+            Solution solution = new Solution(input);
+            System.out.println(solution.calculatePoints());
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
