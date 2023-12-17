177b32abd455f0d7286cc5ea04d9c7f32aa97359
ShevinuM <nsmcooray@mun.ca>
2023-12-17 19:28:40 -0330

    Formatted Code

--- Day_13/sol.py
+++ Day_13/sol.py
@@ -21,7 +21,7 @@ def parseInput(filename):
         for item in rows:
             item_transposed = list(map(list, zip(*item)))
             cols.append(item_transposed)
-     
+
         for e in range(len(rows)):
             for i in range(len(rows[e])):
                 bs = "".join(rows[e][i])
@@ -31,37 +31,45 @@ def parseInput(filename):
                 cols[e][i] = bs
         return rows, cols, len(rows)
 
-"""
-    item -> ['101100110', '001011010', '110000001', '110000001', '001011010', '001100110', '101011010']
-"""
-def getSum(item):
+
+def getAbove(item, part):
     r = 1
     while r < len(item):
         # above -> ['101100110', '001011010', '110000001']
         # below -> ['110000001', '001011010', '001100110', '101011010']
-        above = item[r-1::-1]
+        above = item[r - 1 :: -1]
         below = item[r:]
         n = len(above) if len(above) < len(below) else len(below)
-        # x, y ->  '110000001', '110000001'
-        sum = 0 
-        for x, y in zip(above[:n], below[:n]):
-            # a, b -> '1', '1'
-            for a, b in zip(x, y):
-                if abs(int(a) - int(b)) == 1:
-                    sum += 1
-        if sum == 1: 
-            return len(above) 
+        if part == 2:
+            # x, y ->  '110000001', '110000001'
+            sum = 0
+            for x, y in zip(above[:n], below[:n]):
+                # a, b -> '1', '1'
+                for a, b in zip(x, y):
+                    if abs(int(a) - int(b)) == 1:
+                        sum += 1
+            if sum == 1:
+                return len(above)
+        else:
+            if above[:n] == below[:n]:
+                return len(above)
         r += 1
-    return 0 
+    return 0
 
-rows, cols, nInputs = parseInput("input.txt")
-res = 0 
-for i in range(nInputs):
-    row = rows[i]
-    col = cols[i]
-    row_sum = getSum(row)
-    col_sum = getSum(col)
-    if row_sum != 0 and col_sum != 0:
-        raise Exception("Invalid input")
-    res += 100*row_sum + col_sum
-print(res)
+
+def getSum(filename, part):
+    rows, cols, nInputs = parseInput(filename)
+    res = 0
+    for i in range(nInputs):
+        row = rows[i]
+        col = cols[i]
+        row_sum = getAbove(row, part)
+        col_sum = getAbove(col, part)
+        if row_sum != 0 and col_sum != 0:
+            raise Exception("Invalid input")
+        res += 100 * row_sum + col_sum
+    return res
+
+
+print("Part 1: ", getSum("input.txt", 1))
+print("Part 2: ", getSum("input.txt", 2))
