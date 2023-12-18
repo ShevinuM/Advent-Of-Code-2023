9da305d69391bf1f2f93b32e63f5c72d8b269ef5
ShevinuM <nsmcooray@mun.ca>
2023-12-17 21:54:07 -0330

    Completed Day 14 Part 1

--- Day_14/sol.py
+++ Day_14/sol.py
@@ -1,38 +1,51 @@
 def parseInput(filename):
-    input = [list(line.strip().replace('\n', '')) for line in open(filename, 'r')] 
+    input = [list(line.strip().replace("\n", "")) for line in open(filename, "r")]
     transposed = list(map(list, zip(*input)))
     return input, transposed
 
+
 def tilt(input, transposed):
     for i in range(len(transposed)):
-        line = transposed[i] # ['.', '0', '.', '.', '.', '#', '0', '.', '.', '0]
         l = 0
-        while line[l] == '#':
-            l+=1
+        while transposed[i][l] == "#":
+            l += 1
         r = l + 1
-        c = 1 if line[l] == 'O' else 0
+        c = 1 if transposed[i][l] == "O" else 0
         while r <= len(transposed[i]):
-            if r == len(transposed[i]) or transposed[i][r] == '#':
-                for j in range(r-l):
+            if r == len(transposed[i]) or transposed[i][r] == "#":
+                for j in range(r - l):
                     if j < c:
-                        transposed[i][l+j] = 'O'
+                        transposed[i][l + j] = "O"
                     else:
-                        transposed[i][l+j] = '.' if transposed[i][l+j] == 'O' else transposed[i][l+j]
-                if r >= len(transposed[i])-1:
+                        transposed[i][l + j] = (
+                            "." if transposed[i][l + j] == "O" else transposed[i][l + j]
+                        )
+                if r >= len(transposed[i]) - 1:
                     break
                 else:
-                    l = r+1
-                    while l < len(transposed[i]) and transposed[i][l] == '#':
-                        l+=1
+                    l = r + 1
+                    while l < len(transposed[i]) and transposed[i][l] == "#":
+                        l += 1
                     if l >= len(transposed[i]):
                         break
                     r = l + 1
-                    c = 1 if transposed[i][l] == 'O' else 0
+                    c = 1 if transposed[i][l] == "O" else 0
             else:
-                if transposed[i][r] == 'O':
-                    c+=1
-                r+=1
-    print(transposed)
+                if transposed[i][r] == "O":
+                    c += 1
+                r += 1
+    return list(map(list, zip(*transposed)))[::-1]
 
-input, transposed = parseInput('test_input.txt')
-tilt(input, transposed)
+
+def calcLoad(input):
+    load = 0
+    for r in range(len(input)):
+        for c in range(len(input[r])):
+            if input[r][c] == "O":
+                load += r + 1
+    return load
+
+
+input, transposed = parseInput("input.txt")
+tilted_input = tilt(input, transposed)
+print(calcLoad(tilted_input))
