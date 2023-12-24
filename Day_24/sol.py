4ba42cfa16bd1bc049a3a9d55be4e02a7aef46f9
ShevinuM <nsmcooray@mun.ca>
2023-12-24 12:28:14 -0330

    Reformatted Code

--- Day_24/sol.py
+++ Day_24/sol.py
@@ -2,8 +2,6 @@ from itertools import combinations
 
 
 def intersect(x1, y1, x2, y2, x3, y3, x4, y4):
-    if (x1 == x2 and y1 == y2) or (x3 == x4 and y3 == y4):
-        return False, (None, None)
     denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
 
     if denominator == 0:
