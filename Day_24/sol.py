5b141d3d65b1d3ee09a80375e218c73b94614090
ShevinuM <nsmcooray@mun.ca>
2023-12-26 14:26:26 -0330

    Completed Day 24 part 2

--- Day_24/sol.py
+++ Day_24/sol.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+import sympy as s
 
 
 def intersect(x1, y1, x2, y2, x3, y3, x4, y4):
@@ -26,12 +27,14 @@ def intersect(x1, y1, x2, y2, x3, y3, x4, y4):
 
 
 pInput = []
+pInput2 = []
 with open("input.txt", "r") as f:
     lines = f.readlines()
     for line in lines:
         line = line.strip().replace(" ", "").replace("@", ",")
         x, y, z, vx, vy, vz = line.split(",")
         pInput.append(((int(x), int(y)), (int(vx), int(vy))))
+        pInput2.append(((int(x), int(y), int(z)), (int(vx), int(vy), int(vz))))
 
 res = 0
 combinations = list(combinations(pInput, 2))
@@ -45,4 +48,13 @@ for ((sx1, sy1), (vx1, vy1)), ((sx2, sy2), (vx2, vy2)) in combinations:
     if intersects:
         res += 1
 
-print(res)
+print("Part 1 -> ", res)
+
+# Part 2
+eqs = []
+srx, sry, srz, vrx, vry, vrz = s.symbols("srx, sry, srz, vrx, vry, vrz")
+for (sx, sy, sz), (vx, vy, vz) in pInput2:
+    eqs.append((srx - sx) * (vy - vry) - (sry - sy) * (vx - vrx))
+    eqs.append((sry - sy) * (vz - vrz) - (srz - sz) * (vy - vry))
+a = s.solve(eqs)
+print(a[0][srx] + a[0][sry] + a[0][srz])
